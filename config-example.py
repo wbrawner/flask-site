4054d0041bdf87e36966f67020ab66fa6f0ebe63
Billy Brawner <billybrawner@gmail.com>
2016-03-09 19:48:37 -0600

    Updated sample config for MySQL setup

--- config-example.py
+++ config-example.py
@@ -1,4 +1,7 @@
-DATABASE = '/tmp/flask-site.db'
+MYSQL_USER = 'user'
+MYSQL_PASSWORD = 'password'
+MYSQL_HOST  'localhost'
+MYSQL_DB = 'database'
 DEBUG = True
 SECRET_KEY = 'development key'
 USERNAME = 'username'
